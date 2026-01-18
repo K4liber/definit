9226a975892d3d69781a471c052f62a98e6980a2
K4liber <janbielecki94@gmail.com>
2026-01-18 13:01:51 +0100

    init

--- src/definit/definition/definition_key.py
+++ src/definit/definition/definition_key.py
@@ -10,17 +10,17 @@ class DefinitionKey:
     field: Field
     sub_categories: tuple[str, ...] = ()
 
-    def __hash__(self) -> int:
-        return hash(self.uid)
-
     @cached_property
     def uid(self) -> str:
-        fixed_name = self.name.replace(" ", "_").replace("'", "").replace("-", "_").lower()
-        return "/".join([self.field, *self.sub_categories, fixed_name])
+        return "/".join([self.field, self._fixed_name])
+
+    @cached_property
+    def full_path(self) -> str:
+        return "/".join([self.field, *self.sub_categories, self._fixed_name])
 
     @staticmethod
-    def from_uid(uid: str) -> "DefinitionKey":
-        parts = uid.split("/")
+    def from_full_path(full_path: str) -> "DefinitionKey":
+        parts = full_path.split("/")
         field = Field(parts[0])
         name = parts[-1]
         sub_categories = tuple(parts[1:-1])
@@ -31,3 +31,15 @@ class DefinitionKey:
             phrase = self.name
 
         return f"[{phrase}]({self.uid})"
+
+    def get_index_reference(self) -> str:
+        return f"[{self.name}]({self.full_path})"
+
+    # Internal methods
+
+    def __hash__(self) -> int:
+        return hash(self.uid)
+
+    @cached_property
+    def _fixed_name(self) -> str:
+        return self.name.replace(" ", "_").replace("'", "").replace("-", "_").lower()
